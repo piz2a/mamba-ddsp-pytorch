0718079e12c4b02921860139c38dc344745f99cd
piz2a <zzang-9@naver.com>
2026-07-07 04:49:16 +0000

    Transformer

--- handcode-transformer/handcode-transformer.py
+++ handcode-transformer/handcode-transformer.py
@@ -21,7 +21,7 @@ dropout = 0.2
 torch.manual_seed(1337)
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('input.txt', 'r', encoding='utf-8') as f:
+with open('/workspace/handcode-transformer/input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
@@ -75,12 +75,12 @@ class Head(nn.Module):
         self.dropout = nn.Dropout(dropout)
     
     def forward(self, x):
-        B, T, C = x.shape
+        B, T, C = x.shape  # T <= block_size
         k = self.key(x)   # (B, T, hs)
         q = self.query(x) # (B, T, hs) 
         v = self.value(x) # (B, T, hs)
         wei = q @ k.transpose(-2, -1) * k.shape[-1]**-0.5  # (B, T, hs) @ (B, hs, T) -> (B, T, T)
-        wei = wei.masked_fill(self.tril == 0, float("-inf"))
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))  # truncate self.tril for edge case T < block_size
         wei = F.softmax(wei, dim=1)  # (B, T, T)
         wei = self.dropout(wei)
         out = wei @ v  # (B, T, T) @ (B, T, hs) -> (B, T, hs)
@@ -90,6 +90,7 @@ class Head(nn.Module):
 class MultiHeadAttention(nn.Module):
     """ Attention """
     def __init__(self, num_heads, head_size):
+        super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(head_size * num_heads, n_embd)
         self.dropout = nn.Dropout()
@@ -118,25 +119,53 @@ class Block(nn.Module):
     """ A single transformer block """
     def __init__(self, n_embd, n_head):
         super().__init__()
-        ...
+        head_size = n_embd // n_head
+        self.mha = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
     
     def forward(self, x):
-        ...
+        x = x + self.mha(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
 
 
 class GPTLanguageModel(nn.Module):
     def __init__(self):
         super().__init__()
-        ...
-    
-    def _init_weights(self, module):
-        ...
+        self.token_embedding = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding = nn.Embedding(block_size, n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd)
+        self.lm_head = nn.Linear(n_embd, vocab_size)
     
     def forward(self, idx, targets=None):
-        ...
+        B, T = idx.shape
+        tok_emb = self.token_embedding(idx)  # (B, T, C)
+        pos_emb = self.position_embedding(torch.arange(T, device=device))  # (T, C)
+        x = tok_emb + pos_emb  # (B, T, C)
+        x = self.blocks(x)  # (B, T, C)
+        x = self.ln_f(x)  # (B, T, C)
+        logits = self.lm_head(x)  # (B, T, vocab_size)
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+        return logits, loss
     
     def generate(self, idx, max_new_tokens):
-        ...
+        for _ in range(max_new_tokens):
+            idx_cond = idx[:, -block_size:]  # crop context if needed
+            logits, loss = self(idx_cond)
+            logits = logits[:, -1, :]  # focus only on the last time step
+            probs = F.softmax(logits, dim=-1)  # (B, C)
+            idx_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
+            idx = torch.cat((idx, idx_next), dim=1)  # append sampled index to the running sequence
+        return idx
 
 
 model = GPTLanguageModel()
